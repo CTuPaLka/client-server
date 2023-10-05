1681fcd2468460696b4725c473479507aa3c15cd
chicory <grifon.rudenko@yandex.com>
2023-10-05 19:45:09 +0300

    eventSource

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import EventSourcing from './EventSource';
 import LongPollings from './LongPollings';
 
 function App() {
   return (
     <div className="App">
-      <LongPollings />
+      <EventSourcing />
     </div>
   );
 }
